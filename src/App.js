66e858db15d900d682d89ec01612122f2e5644df
queen-x-vee <valentinabuoro4@gmail.com>
2022-10-20 22:30:35 +0100

    feat: built the page skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="top-section">
+      <div className="top-container">
+        <div className="top-content">
+          <div className="profile">
+            <div className="profile-picture">
+              <img id="profile__img" alt="profilepicture" src='../profilePicture.jpeg' />
+            </div>
+            <div id="twitter">valentinaBuoro</div>
+          </div>
+          <ul className="links">
+          <li>
+              <Link className="link">Twitter link</Link>
+            </li>
+            <li>
+              <Link className="link">Zuri Team</Link>
+            </li>
+            <li>
+              <Link className="link">Zuri Books</Link>
+            </li>
+            <li>
+              <Link className="link">Python Books</Link>
+            </li>
+            <li>
+              <Link className="link">Background Check for coders</Link>
+            </li>
+            <li>
+              <Link className="link">Design books</Link>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+      <div className="bottom-container">
+        <div className="bottom-content">
+          <div className="socials">
+          <img src= '../public/slack.png' alt='slack'/>
+          <img src = '../public/Icon.png' alt ='github'/>
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
