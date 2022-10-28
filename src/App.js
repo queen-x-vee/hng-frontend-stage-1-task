0d6628ba99ec4d28a980f2155ed4f613cf9a33f8
queen-x-vee <valentinabuoro4@gmail.com>
2022-10-28 10:37:58 +0100

    created a data file

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./index.css";
+import { UserDetails, userLinks } from "./data/data";
 
 const App = () => {
   return (
     <div className="top-section">
       <div className="top-container">
         <div className="top-content">
-          <div className="profile">
-            <div className="profile-picture">
-              <img id="profile__img" alt="profilepicture" src='../profilePicture.jpeg' />
-            </div>
-            <div id="twitter">valentinaBuoro</div>
-          </div>
+          {UserDetails.map((user) => {
+            return (
+              <div className="profile">
+                <div className="profile-picture">
+                  <img
+                    id="profile__img"
+                    alt="profilepicture"
+                    src="../profilePicture.jpeg"
+                  />
+                </div>
+                <div id={user.id}>{user.userNameTwitter}</div>
+                <p id="slack">{user.userNameSlack}</p>
+              </div>
+            );
+          })}
+
           <ul className="links">
-          <li>
-              <Link className="link">Twitter link</Link>
-            </li>
-            <li>
-              <Link className="link">Zuri Team</Link>
-            </li>
-            <li>
-              <Link className="link">Zuri Books</Link>
-            </li>
-            <li>
-              <Link className="link">Python Books</Link>
-            </li>
-            <li>
-              <Link className="link">Background Check for coders</Link>
-            </li>
-            <li>
-              <Link className="link">Design books</Link>
-            </li>
-            
+            {userLinks.map((link) => {
+              return (
+                <>
+                  <li>
+                    <Link className="link" id={link.id} to={link.link}>{link.title}</Link>
+                  </li>
+                </>
+              );
+            })}
           </ul>
         </div>
       </div>
       <div className="bottom-container">
         <div className="bottom-content">
           <div className="socials">
-          <img src= '../public/slack.png' alt='slack'/>
-          <img src = '../public/Icon.png' alt ='github'/>
+            <img src="../public/slack.png" alt="slack" />
+            <img src="../public/Icon.png" alt="github" />
           </div>
-          
         </div>
       </div>
     </div>
---
+++ src/data/data.jsx
@@ -0,0 +1,39 @@
+export const UserDetails = [
+    {
+        id: 'twitter',
+        userNameTwitter: 'valentinaBuoro',
+    
+        userNameSlack: '@bigTeenz'
+    }
+];
+
+export const userLinks = [
+    {
+        id: 'twitter',
+        title: 'Twitter-Link',
+        link: 'https://twitter.com/valentinaBuoro'
+    },
+    {   id: 'zuri__team',
+        title: 'Zuri Team',
+        link: 'https://training.zuri.team'
+    },
+    {   id: 'books',
+        title: 'Zuri Books',
+        link: ' http://books.zuri.team'
+    },
+    {
+        id: 'book__python',
+        title: 'Python Books',
+        link: 'https://books.zuri.team/python-for-beginners?ref_id=elijah-igbokah'
+    },
+    {
+        id: 'pitch',
+        title: 'Background Check for Coders',
+        link: 'https://background.zuri.team'
+    },
+    {
+        id: 'book__design',
+        title: 'Design Books',
+        link: 'https://books.zuri.team/design-rules'
+    }
+];  
